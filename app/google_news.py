41e8240561b0efbe1090a2f7e274952708038270
Daniel <dgranuzzo@yahoo.com>
2025-11-01 00:45:34 -0300

    Change news topic from 'Flamengo' to 'Copa Libertadores'

--- app/google_news.py
+++ app/google_news.py
@@ -26,7 +26,7 @@ def get_google_news(search_text, language='pt'):
 #g_news=g_news.get__links()
 
 if __name__=='__main__':
-    news="Flamengo"
+    news="Copa Libertadores"
     r = get_google_news(news)
     print(r)
     print(type(r))
